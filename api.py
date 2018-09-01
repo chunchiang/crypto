3902aa0dd73e2e19dcff7ecbc2bab5eee20d7621
Chunchiang Wang <chunchiang@hotmail.com>
2018-09-01 11:02:50 -0700

    Add URLError as the exceptions to be retried when getting prices from the exchanges.

--- api.py
+++ api.py
@@ -85,17 +85,15 @@ class API(threading.Thread):
 
                 # Get new prices
                 log.debug('Get price updates')
-                for i in xrange(3):
+                for i in xrange(10):
                     try:
                         my_tickers_price_history, my_price_time = self.get_prices(self.my_tickers)
-                    except urllib2.HTTPError as e:
+                    except (urllib2.HTTPError, urllib2.URLError) as e:
+                        # Raise HTTP Error if still fails after 3 retries
+                        if i > 3:
+                            raise
                         log.warning('Unable to get price from {0} in {1} try!'.format(self.exchange, i))
                         log.warning(e.message)
-                        
-                        # Raise HTTP Error if still fails after 3 retries
-                        if i >= 3:
-                            raise
-                            
                     # Break loop if no HTTPError while getting price 
                     else:
                         break
